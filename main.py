ab64bdc1ae0b3ece0bb1891448097f6f0010be92
Paulius Piekarskis <motorpaul@hotmail.com>
2023-04-25 12:59:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int):
+        f = str(x)
+        if f == f[::-1]:
+            return ('Is a Palendrome')
+        return ('Is not a Palendrome')
+
+s1 = Solution()
+
+print(s1.isPalindrome(121))
+print(s1.isPalindrome(569))
+print(s1.isPalindrome(565))
+print(s1.isPalindrome(151151))
